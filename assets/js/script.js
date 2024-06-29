75a2ccf38958047198d3cacaab6dca44480133d0
Doru Muresanu <doru.muresanu@yahoo.ie>
2024-06-29 20:28:12 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function moveSpaceship() {
+
+}
+
+function shoot() {
+
+}
+
+function moveProkectile() {
+
+}
+
+function createAlien() {
+
+}
+
+function moveAlien() {
+
+}
+
+function checkCollision() {
+
+}
+
+function loseLife() {
+
+}
+
+function gameLoop() {
+    
+}
